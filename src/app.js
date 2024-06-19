b46d4e77ffea1794304a816ed86b1249cf477278
Svetlana M <meritee@gmx.de>
2024-06-19 16:55:01 +0200

    project settings

---
+++ src/app.js
@@ -0,0 +1,16 @@
+
+import styled from 'styled-components';
+
+const Div = styled.div`
+	text-align: center
+`
+
+export const App = () => {
+	return (
+		<Div>
+			<h1>App</h1>
+			<Div>123</Div>
+			<i className="fa fa-camera-retro fa-2x"></i>
+		</Div>
+	);
+};
